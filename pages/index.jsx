4c6847a817cfd9b8c65c424909d99fad703fee4f
Varad Sharma <varad.code19@gmail.com>
2023-09-29 12:46:56 +0530

    fix UI and my life without coffee#1

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,7 +9,7 @@ const Home = () => {
     <Header
       title={"Welcome To the Extra Quadrata Tech Fest V2"}
       description={`We are invigorated and thrilled to announce that our school, DPS Kolar, is hosting the Extra Quadrata Tech Fest on the 5th of October. Our event encompasses a diverse array of sub-events, ranging from gaming to programming.`}
-      video={{ src: '/Dps Cover.mp4' }} />
+      video={{ src: '/retroBG.png' }} />
     <Venue />
     <InfoSection /> 
     <Register />
