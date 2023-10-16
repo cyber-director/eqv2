b434ebe9a195bef2a23aa0fa0181e3ea54e9058e
Varad Sharma <varad.code19@gmail.com>
2023-10-16 18:32:06 +0530

    register api speed

--- pages/api/register.js
+++ pages/api/register.js
@@ -1,84 +1,78 @@
-import Registration from '../../models/registration';
-import Team from '../../models/team';
-import Member from '../../models/member';
-import {
-    checkRegistration,
-    verifyEmail
-} from '../../utils/emails';
-import {
-    v4 as uuid
-} from 'uuid';
-import Verify from '../../models/verify';
-import connect from '../../utils/mongoDBConnector';
-import eventValidator from '../../utils/eventValidator';
-import member from '../../models/member';
+import Registration from "../../models/registration";
+import Team from "../../models/team";
+import Member from "../../models/member";
+import Verify from "../../models/verify";
+import connect from "../../utils/mongoDBConnector";
+import { checkRegistration, verifyEmail } from "../../utils/emails";
+import { v4 as uuid } from "uuid";
+import eventValidator from "../../utils/eventValidator";
+
+const createMember = async (memberData) => {
+    const existingMember = await Member.findOne({ email: memberData.email });
+    if (existingMember) {
+        throw new Error(`${memberData.email} is already registered`);
+    }
+
+    const newMember = new Member(memberData);
+    await newMember.save();
+    return newMember;
+};
 
 const Handler = async (req, res) => {
-    const {
-        school,
-        event,
-        teamName,
-        members
-    } = req.body;
+    const { school, event, teamName, members } = req.body;
+
     try {
         await connect();
+
         if (req.method === "POST") {
             eventValidator(event, members);
-            // Un-comment When registerations closed
-            // return res.status(400).json({error:"Registrations have been closed"})
-            for (let i = 0; i < members.length; i++) {
-                let exist = await Member.findOne({
-                    email: members[i].email
-                });
-                if (exist) {
-                    return res.status(400).json({
-                        error: `${members[i].email} is already registered`
-                    });
+
+            const mems = [];
+            const existingMembers = [];
+
+            for (const memberData of members) {
+                try {
+                    const newMember = await createMember(memberData);
+                    mems.push(newMember._id);
+                } catch (error) {
+                    existingMembers.push(error.message);
                 }
             }
-            let mems = [];
 
-            for (let i = 0; i < members.length; i++) {
-                let member = new Member(members[i]);
-
-                await member.save();
-                mems = [...mems, member._id];
+            if (existingMembers.length > 0) {
+                return res
+                    .status(400)
+                    .json({ error: existingMembers.join(", ") });
             }
-            let team = new Team({
-                name: req.body.teamName,
-                school: school,
-                members: mems
-            });
+
+            const team = new Team({ name: teamName, school, members: mems });
             await team.save();
-            console.log(team);
-            let registration = new Registration({
-                team: team._id,
-                event: event
-            });
+
+            const registration = new Registration({ team: team._id, event });
             await registration.save();
-            for (let i = 0; i < members.length; i++) {
-                let member = await Member.findOne({
-                    email: members[i].email
+
+            for (const memberData of members) {
+                const member = await Member.findOne({
+                    email: memberData.email,
                 });
-                let verify = new Verify({
-                    code: uuid(),
-                    member: member._id
-                });
+
+                const verify = new Verify({ code: uuid(), member: member._id });
                 await verify.save();
+
                 await verifyEmail(member, registration._id, verify.code, event);
                 await checkRegistration(member, registration._id, event);
             }
-            return res.status(200).json({
-                error: 'Team registered'
-            });
+
+            return res.status(200).json({ message: "Team registered" });
         }
     } catch (err) {
-        console.log(err)
-        return res.status(500).json({
-            error: "We can't process your request at the moment. Please try again later"
-        });
+        console.error(err);
+        return res
+            .status(500)
+            .json({
+                error: "We can't process your request at the moment. Please try again later",
+            });
     }
-
-}
+};
 
 export default Handler;
