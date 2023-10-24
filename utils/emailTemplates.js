9188ccaa13d9c10cb6a727ddc932b25e2562d0c7
Varad Sharma <Varad Sharma>
2023-10-24 11:56:28 +0530

    mail fix lol

--- utils/emailTemplates.js
+++ utils/emailTemplates.js
@@ -1,14 +1,13 @@
 export const verifyEmail = (member, event, link, link2) => {
-  return `<div style="background-color:#011620; padding:2rem;margin:0">
-  <div>
-    <img style="width:200px; border-radius:100%" src="https://res.cloudinary.com/dx73dzg3g/image/upload/v1665937862/socizilla/Headway_1_nqxohs.png">
-  </div>
+    return `<div style="background-color:#011620; padding:2rem;margin:0">
   <div style="color:white">
   <h3>Greetings, ${member.name}</h3>
     <p>
       Your registeration process for the ${event} event has been initiated. To verify your registration, please click on the button below.  
       </p>
     <a  style="text-decoration:none;" href='${link}'>Verify Email </a>
+    <p style="font-size:14px; margin-top:4rem;"><b>OR</b></p>
+    <p>${link}</p>
     <p style="font-size:14px; margin-top:4rem;">
       In case you didn't register for the ${event} event, click on the button below to stop your registration process.  
     </p>
@@ -19,21 +18,20 @@ export const verifyEmail = (member, event, link, link2) => {
 <div style="background-color:black; text-align:center; padding:2rem; color:white;"> 
   <p style="margin-top:0"> © 2022 Extra Quadrata, Headway, DPS Kolar Road Bhopal
   <p style="color:white; text-decoration:none">For more information, click <a style="color:white" href=${process.env.ADDRESS}>here</a></p> 
-</div>`
-}
+</div>`;
+};
 
 export const checkRegistrationStatus = (member, eventName, link, link2) => {
-  return `
+    return `
     <div style="background-color:#011620; padding:2rem;margin:0">
-  <div style="display: flex; justify-content:center">
-    <img style="width:200px; border-radius:100%" src="https://res.cloudinary.com/dx73dzg3g/image/upload/v1665937862/socizilla/Headway_1_nqxohs.png">
-  </div>
   <div style="color:white">
   <h3>Greetings, ${member.name}</h3>
     <p>
       Your registeration process for the ${eventName} event has been initiated. To check your registration status, please click on the button below.  
       </p>
-    <a style="background-image:linear-gradient(to right, #D60270, #9B4F96, #0038A8); padding:1.5rem; font-weight: 900; box-shadow: 0 0 20px rgba(0,0,0,0.9); position:relative; display:inline-block; color:white; border:none; text-decoration:none; margin:1rem 0; border-radius:1rem" href=${link}>Check Status</a> 
+    <a style="background-image:linear-gradient(to right, #D60270, #9B4F96, #0038A8); padding:1.5rem; font-weight: 900; box-shadow: 0 0 20px rgba(0,0,0,0.9); position:relative; display:inline-block; color:white; border:none; text-decoration:none; margin:1rem 0; border-radius:1rem" href=${link}>Check Status</a> \
+    <p style="font-size:14px; margin-top:4rem;"><b>OR</b></p>
+    <p>${link}</p>
     <p style="font-size:14px; margin-top:4rem;">
       In case you didn't register for the ${eventName} event, click on the button below to stop your registration process.  
     </p>
@@ -44,14 +42,10 @@ export const checkRegistrationStatus = (member, eventName, link, link2) => {
 <div style="background-color:black; text-align:center; padding:2rem; color:white;"> 
   <p style="margin-top:0"> © 2022 Extra Quadrata, Headway, DPS Kolar Road Bhopal
   <p style="color:white; text-decoration:none">For more information, click <a style="color:white" href=${process.env.ADDRESS}>here</a></p> 
-</div>`
-}
+</div>`;
+};
 export const registrationCanceled = (member, eventName) => {
-
-  return `<div style="background-color:#011620; padding:2rem;margin:0">
-  <div>
-    <img style="width:200px; border-radius:100%" src="https://res.cloudinary.com/dx73dzg3g/image/upload/v1665937862/socizilla/Headway_1_nqxohs.png">
-  </div>
+    return `<div style="background-color:#011620; padding:2rem;margin:0">
   <div style="color:white">
   <h3>Greetings, ${member.name}</h3>
     <p>
@@ -61,15 +55,11 @@ export const registrationCanceled = (member, eventName) => {
 <div style="background-color:black; text-align:center; padding:2rem; color:white;"> 
   <p style="margin-top:0"> © 2022 Extra Quadrata, Headway, DPS Kolar Road Bhopal
   <p style="color:white; text-decoration:none">For more information, click <a style="color:white" href=${process.env.ADDRESS}>here</a></p> 
-</div>`
-
-}
+</div>`;
+};
 export const registrationAccepted = (team, member, eventName, link) => {
-  return `
+    return `
     <div style="background-color:#011620; padding:2rem;margin:0">
-  <div style="display: flex; justify-content:center">
-    <img style="width:200px; border-radius:100%" src="https://res.cloudinary.com/dx73dzg3g/image/upload/v1665937862/socizilla/Headway_1_nqxohs.png">
-  </div>
   <div style="color:white">
     <h3>Congratuations, ${member.name}</h3>
     <p>
@@ -89,15 +79,12 @@ export const registrationAccepted = (team, member, eventName, link) => {
 <div style="background-color:black; text-align:center; padding:2rem; color:white;"> 
   <p style="margin-top:0"> © 2022 Extra Quadrata, Headway, DPS Kolar Road Bhopal</p>
   <p style="color:white; text-decoration:none">For more information, click <a style="color:white" href=${process.env.ADDRESS}>here</a></p> 
-</div>`
-}
+</div>`;
+};
 
 export const registrationDenied = (member, eventName, link) => {
-  return `
+    return `
   <div style="background-color:#011620; padding:2rem;margin:0">
-    <div style="display: flex; justify-content:center">
-      <img style="width:200px; border-radius:100%" src="https://res.cloudinary.com/dx73dzg3g/image/upload/v1665937862/socizilla/Headway_1_nqxohs.png">
-    </div>
     <div style="color:white">
       <h3>Greeting, ${member.name}</h3>
       <p>
@@ -109,5 +96,5 @@ export const registrationDenied = (member, eventName, link) => {
   <div style="background-color:black; text-align:center; padding:2rem; color:white;"> 
     <p style="margin-top:0"> © 2022 Extra Quadrata, Headway, DPS Kolar Road Bhopal </p>
     <p style="color:white; text-decoration:none">For more information, click <a style="color:white" href=${process.env.ADDRESS}>here</a></p> 
-</div>`
-}
+</div>`;
+};
