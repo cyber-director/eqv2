5d1be7babd93cbfab62e3a2e56e4d4032a77f942
Varad Sharma <117466176+Varad-Codes@users.noreply.github.com>
2023-11-17 13:02:28 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -10,14 +10,14 @@ function MyApp({ Component, pageProps }) {
             <meta name="robots" content="all" />
             <meta name='description'
                   content="“Technology like art is a soaring exercise of the human imagination.” To be steady with our motion of development, it is vital for the youth to grasp every facet of technology and innovation.
-        DPS Kolar Road has taken an initiative to contribute to this field. Extraquadrata would be our very first annual tech meet up hosted by and for high schoolers which will be held on the 14th of November.
+        DPS Kolar Road has taken an initiative to contribute to this field. Extraquadrata would be our very first annual tech meet up hosted by and for high schoolers which will be held on the 5th of December.
         Our competitions would test the participant’s creativity and logical thinking to response time and sheer skill for tech savvies to people who crave to be."></meta>
             <meta name="og:image" content="/logo.png" />
             <link rel='icon' href='/logo.png' />
             <link rel='apple-touch-icon' href='/logo.png' />
             <meta name='og:description'
                   content="“Technology like art is a soaring exercise of the human imagination.” To be steady with our motion of development, it is vital for the youth to grasp every facet of technology and innovation.
-        DPS Kolar Road has taken an initiative to contribute to this field. Extraquadrata would be our very first annual tech meet up hosted by and for high schoolers which will be held on the 14th of November.
+        DPS Kolar Road has taken an initiative to contribute to this field. Extraquadrata would be our very first annual tech meet up hosted by and for high schoolers which will be held on the 5th of December.
         Our competitions would test the participant’s creativity and logical thinking to response time and sheer skill for tech savvies and people who crave to be."></meta>
         </Head>
         <div style={{minHeight:'80vh'}}>
